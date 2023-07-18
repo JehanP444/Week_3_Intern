cf70ac2411a6c44b27cc40e0c78d6c58fa04fc5b
Mufti Muhammad Nur Prabowo <mprabowo@asuransiastra.com>
2023-07-18 17:56:55 +0700

    Skenario lihat dan pilih kelas

--- cypress/e2e/webKuncie/tes.js
+++ cypress/e2e/webKuncie/tes.js
@@ -1,7 +1,23 @@
 import {Given,When,Then} from "@badeball/cypress-cucumber-preprocessor";
+import { when } from "cypress/types/jquery";
 
 Given(/^Jehan login$/,() => {
   cy.visit('https://www.kuncie.com/premium/')
 
   cy.get("[class='login-header-menu btn']").click()
+
+  cy.get("[class='login-username']").type('jehanpratama911@gmail.com')
+  cy.get("[class='login-password']").type('Anaknusa404')
+
+  cy.get("[class='login-submit']").click()
+  cy.wait(5000)
+});
+
+When(/^Jehan melihat kelas yang tersedia$/, () => {
+  cy.get("[data-id='a6ac833']").click()
+  cy.wait(3000)
+});
+
+When(/^Jehan memilih kelas$/, () => { 
+  cy.get("[class='course-button medium-btn']").eq(1).click()
 });
--- cypress/e2e/webKuncie/testJehan.feature
+++ cypress/e2e/webKuncie/testJehan.feature
@@ -3,3 +3,7 @@ Feature:  login kuncie
 Scenario: Jehan mengunjungi website kuncie untuk melakukan login
 
 Given Jehan login
+
+When Jehan melihat kelas yang tersedia
+
+When Jehan memilih kelas
